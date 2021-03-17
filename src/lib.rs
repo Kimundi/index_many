049fe0da98973c57b03a223649cf5dd4ea5d1c86
Marvin Löbel <loebel.marvin@gmail.com>
2021-03-17 21:40:21 +0100

    shared variant

--- src/lib.rs
+++ src/lib.rs
@@ -39,12 +39,22 @@ fn check_indices(indices: &[usize], len: usize) {
     }
 }
 
-pub fn multi_index<'a, T, const N: usize>(
+pub fn index_many<'a, T, const N: usize>(slice: &'a [T], indices: [usize; N]) -> [&'a T; N] {
+    check_indices(&indices, slice.len());
+    unsafe {
+        let mut arr: [*const T; N] = [std::ptr::null(); N];
+        for (dst, idx) in arr.iter_mut().zip(indices.iter().copied()) {
+            *dst = slice.get_unchecked(idx);
+        }
+        arr.map(|v| &*v)
+    }
+}
+
+pub fn index_many_mut<'a, T, const N: usize>(
     slice: &'a mut [T],
     indices: [usize; N],
 ) -> [&'a mut T; N] {
     check_indices(&indices, slice.len());
-
     unsafe {
         let mut arr: [*mut T; N] = [std::ptr::null_mut(); N];
         for (dst, idx) in arr.iter_mut().zip(indices.iter().copied()) {
@@ -61,7 +71,7 @@ mod tests {
     #[test]
     fn test_normal() {
         let mut v = vec![1, 2, 3, 4, 5];
-        let [a, b, c] = multi_index(&mut v, [0, 2, 4]);
+        let [a, b, c] = index_many_mut(&mut v, [0, 2, 4]);
         *a += 10;
         *b += 100;
         *c += 1000;
@@ -71,14 +81,14 @@ mod tests {
     #[test]
     fn test_empty() {
         let mut v = vec![1, 2, 3, 4, 5];
-        let [] = multi_index(&mut v, []);
+        let [] = index_many_mut(&mut v, []);
         assert_eq!(v, vec![1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn test_single_first() {
         let mut v = vec![1, 2, 3, 4, 5];
-        let [a] = multi_index(&mut v, [0]);
+        let [a] = index_many_mut(&mut v, [0]);
         *a += 10;
         assert_eq!(v, vec![11, 2, 3, 4, 5]);
     }
@@ -86,7 +96,7 @@ mod tests {
     #[test]
     fn test_single_last() {
         let mut v = vec![1, 2, 3, 4, 5];
-        let [a] = multi_index(&mut v, [4]);
+        let [a] = index_many_mut(&mut v, [4]);
         *a += 10;
         assert_eq!(v, vec![1, 2, 3, 4, 15]);
     }
@@ -95,13 +105,13 @@ mod tests {
     #[should_panic]
     fn test_oob_nonempty() {
         let mut v = vec![1, 2, 3, 4, 5];
-        multi_index(&mut v, [5]);
+        index_many_mut(&mut v, [5]);
     }
 
     #[test]
     #[should_panic]
     fn test_oob_empty() {
         let mut v: Vec<i32> = vec![];
-        multi_index(&mut v, [0]);
+        index_many_mut(&mut v, [0]);
     }
 }
