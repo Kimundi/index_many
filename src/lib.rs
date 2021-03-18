1c54aa3b3ebe4c27895df5069736b02226b44e5c
Marvin Löbel <loebel.marvin@gmail.com>
2021-03-18 11:30:45 +0100

    add shared reference tests

--- src/lib.rs
+++ src/lib.rs
@@ -151,7 +151,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_normal() {
+    fn test_mut_normal() {
         let mut v = vec![1, 2, 3, 4, 5];
         let [a, b, c] = index_many_mut(&mut v, [0, 2, 4]);
         *a += 10;
@@ -161,14 +161,31 @@ mod tests {
     }
 
     #[test]
-    fn test_empty() {
+    fn test_ref_normal() {
+        let v = vec![1, 2, 3, 4, 5];
+        let [a, b, c] = index_many(&v, [0, 2, 4]);
+        assert_eq!(a, &1);
+        assert_eq!(b, &3);
+        assert_eq!(c, &5);
+        assert_eq!(v, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_mut_empty() {
         let mut v = vec![1, 2, 3, 4, 5];
         let [] = index_many_mut(&mut v, []);
         assert_eq!(v, vec![1, 2, 3, 4, 5]);
     }
 
     #[test]
-    fn test_single_first() {
+    fn test_ref_empty() {
+        let v = vec![1, 2, 3, 4, 5];
+        let [] = index_many(&v, []);
+        assert_eq!(v, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_mut_single_first() {
         let mut v = vec![1, 2, 3, 4, 5];
         let [a] = index_many_mut(&mut v, [0]);
         *a += 10;
@@ -176,7 +193,15 @@ mod tests {
     }
 
     #[test]
-    fn test_single_last() {
+    fn test_ref_single_first() {
+        let v = vec![1, 2, 3, 4, 5];
+        let [a] = index_many(&v, [0]);
+        assert_eq!(a, &1);
+        assert_eq!(v, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_mut_single_last() {
         let mut v = vec![1, 2, 3, 4, 5];
         let [a] = index_many_mut(&mut v, [4]);
         *a += 10;
@@ -184,26 +209,58 @@ mod tests {
     }
 
     #[test]
+    fn test_ref_single_last() {
+        let v = vec![1, 2, 3, 4, 5];
+        let [a] = index_many(&v, [4]);
+        assert_eq!(a, &5);
+        assert_eq!(v, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
     // #[should_panic(expected = "index 5 is out of bounds")]
     #[should_panic]
-    fn test_oob_nonempty() {
+    fn test_mut_oob_nonempty() {
         let mut v = vec![1, 2, 3, 4, 5];
         index_many_mut(&mut v, [5]);
     }
 
     #[test]
+    // #[should_panic(expected = "index 5 is out of bounds")]
+    #[should_panic]
+    fn test_ref_oob_nonempty() {
+        let v = vec![1, 2, 3, 4, 5];
+        index_many(&v, [5]);
+    }
+
+    #[test]
     // #[should_panic(expected = "index 0 is out of bounds")]
     #[should_panic]
-    fn test_oob_empty() {
+    fn test_mut_oob_empty() {
         let mut v: Vec<i32> = vec![];
         index_many_mut(&mut v, [0]);
     }
 
     #[test]
+    // #[should_panic(expected = "index 0 is out of bounds")]
+    #[should_panic]
+    fn test_ref_oob_empty() {
+        let v: Vec<i32> = vec![];
+        index_many(&v, [0]);
+    }
+
+    #[test]
     // #[should_panic(expected = "indices [3, 1, 9] are not unique or sorted in ascending order")]
     #[should_panic]
-    fn test_unsorted() {
+    fn test_mut_unsorted() {
         let mut v = vec![1, 2, 3, 4, 5];
         index_many_mut(&mut v, [3, 1, 9]);
+    }
+
+    #[test]
+    // #[should_panic(expected = "indices [3, 1, 9] are not unique or sorted in ascending order")]
+    #[should_panic]
+    fn test_ref_unsorted() {
+        let v = vec![1, 2, 3, 4, 5];
+        index_many(&v, [3, 1, 9]);
     }
 }
