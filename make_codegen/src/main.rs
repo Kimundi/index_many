c573f6a13bde36307e2d7b97c5e583347f8dd00f
Marvin Löbel <loebel.marvin@gmail.com>
2021-08-12 15:38:44 +0200

    detect duplicates

--- make_codegen/src/main.rs
+++ make_codegen/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(exit_status_error)]
 
-use std::{path::Path, process::Output};
+use std::{collections::HashMap, path::Path, process::Output};
 
 use tempfile::tempdir;
 
@@ -29,20 +29,14 @@ fn clear_asm(p: &Path) {
 }
 
 fn main() {
-    let output_dir = Path::new("./.codegen");
     let codegen_crate_name = "codegen_crate";
-
-    std::fs::create_dir_all(&output_dir).unwrap();
 
     let tempdir = tempdir().unwrap();
     let tempdir = tempdir.path().join(codegen_crate_name);
     std::fs::create_dir_all(&tempdir).unwrap();
-
     println!("generated into temporary directory {}", tempdir.display());
 
     run_raw("cargo init --lib . --vcs none", &tempdir, false);
-
-    // TODO: Escape path properly
     let crate_path = std::env::current_dir().unwrap();
     let crate_name = crate_path.file_name().unwrap().to_str().unwrap().to_owned();
     let crate_path = crate_path
@@ -64,13 +58,15 @@ fn main() {
     "##,
         codegen_crate_name, crate_name, crate_path
     );
-
     std::fs::write(tempdir.join("Cargo.toml"), toml).unwrap();
     std::fs::write(tempdir.join("src").join("lib.rs"), "").unwrap();
-
     run_raw("cargo build --release", &tempdir, false);
 
+    let output_dir = Path::new("./.codegen");
+    std::fs::create_dir_all(&output_dir).unwrap();
     clear_asm(&output_dir);
+
+    let mut duplicates = HashMap::new();
 
     let header = codegen_test::HEADER;
     for &(i, name, source) in codegen_test::FUNCTIONS {
@@ -83,7 +79,14 @@ fn main() {
         let out = run_raw(&s, &tempdir, true);
 
         let ret = match out.status.exit_ok() {
-            Ok(_) => String::from_utf8(out.stdout).unwrap(),
+            Ok(_) => {
+                let ret = String::from_utf8(out.stdout).unwrap();
+
+                let v: &mut Vec<_> = duplicates.entry(ret.replace(name, "<name>")).or_default();
+                v.push((i, name));
+
+                ret
+            }
             Err(e) => {
                 eprintln!("Error[{}] with `{}`", e.code().unwrap_or(0), s);
                 eprintln!("{}", String::from_utf8(out.stderr).unwrap());
@@ -95,5 +98,18 @@ fn main() {
     }
 
     println!();
+
+    println!("Duplicates:");
+    for v in duplicates.values() {
+        if v.len() > 1 {
+            for (i, e) in v {
+                println!("  {} {}", i, e);
+            }
+            println!();
+        }
+    }
+
+    println!();
+
     println!("DONE");
 }
